350e901b8ed1dc50824180606cb28b2c01622023
tjnt <3m2dyzxu@gmail.com>
2014-11-15 16:05:16 +0900

    not to create a link to mklink.sh

--- mklink.sh
+++ mklink.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 for f in `find . -type d -name '.git' -prune -o -type f -print | \
     sed -e 's/^\.\///g'`
 do
-    if [ $0 = "$PWD/$f" ]; then
+    if [ "`basename $0`" = "$f" ]; then
         continue
     fi
     ln -snfv "$PWD/$f" "$HOME/$f"
