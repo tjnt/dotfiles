d55e565ad78c5f54ecac1c6630eabcce3d58f282
tjnt <3m2dyzxu@gmail.com>
2017-07-01 22:02:29 +0900

    modify

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -245,8 +245,9 @@ awesomebutton = awful.widget.launcher({
 app_table = {
   { "urxvt", "/usr/share/icons/gnome/32x32/apps/terminal.png" },
   { "gvim", "/usr/share/icons/hicolor/48x48/apps/gvim.png" },
+  { "firefox", "/usr/share/icons/hicolor/32x32/apps/firefox-esr.png" },
   -- { "pcmanfm", "/usr/share/icons/gnome/32x32/apps/file-manager.png" },
-  { "firefox", "/home/tjnt/.local/share/icons/hicolor/32x32/apps/firefox-nightly.png" },
+  -- { "firefox", "/home/tjnt/.local/share/icons/hicolor/32x32/apps/firefox-nightly.png" },
   -- { "iceweasel", "/usr/share/icons/hicolor/32x32/apps/iceweasel.png" },
   -- { "icedove", "/usr/share/icons/hicolor/32x32/apps/icedove.png" },
 
