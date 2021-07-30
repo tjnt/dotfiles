c43525831146f76ff1540ab5451adc8a3f75d539
tjnt <3m2dyzxu@gmail.com>
2021-07-30 17:07:16 +0900

    merge clip-sel/del script

--- .local/bin/clip-del.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# used to delete clipboard history from xmonad.
-fzf=~/.zplug/bin/fzf
-export FZF_DEFAULT_OPTS='--layout=reverse --border --inline-info'
-recid=$(clipc --list | $fzf | awk '{print $1}')
-[[ -n $recid ]] \
-    && clipc --delete $recid
--- .local/bin/clip-sel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# used to select clipboard history from xmonad.
-fzf=~/.zplug/bin/fzf
-export FZF_DEFAULT_OPTS='--layout=reverse --border --inline-info'
-recid=$(clipc --list | $fzf | awk '{print $1}')
-[[ -n $recid ]] \
-    && clipc --select $recid \
-    && xclip -o -selection clipboard | xsel --clipboard
